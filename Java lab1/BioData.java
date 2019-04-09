583fdbb4b375be48b5d808f1dbf79b24030f26fe
pirihsahito <49409581+pirihsahito@users.noreply.github.com>
2019-04-09 18:31:46 +0500

    Add files via upload

---
+++ Java lab1/BioData.java
@@ -0,0 +1,11 @@
+class BioData
+{
+	public static void main(String args[])
+	{
+	String n="Pirih Sahitio";
+	int a=19;
+	double p=78.5;
+	char g='A';
+	System.out.print(n+a+p+g);
+}
+}
---
+++ Java lab1/DataCasting.java
@@ -0,0 +1,11 @@
+class DataCasting
+{
+	public static void main(String args[])
+	{
+	double a=50.95;
+	long b=(long)50.95;
+	System.out.println("Double Value="+a);
+	System.out.println("Conversion="+b);
+	System.out.println("Fractional Part="+(a-b));
+	}
+}
